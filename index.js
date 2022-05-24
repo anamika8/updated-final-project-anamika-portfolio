c0dae68125d06688cc3d214c2b343173dd9b8cb3
Anamika Nayak <anayak@pdx.edu>
2022-05-24 15:47:11 -0700

    added navigation to access different section

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var textWrapper = document.querySelector('.ml16');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+// Get all sections
+var allSections = document.querySelectorAll("section");
+
 anime.timeline({ loop: true })
     .add({
         targets: '.ml16 .letter',
@@ -16,3 +19,14 @@ anime.timeline({ loop: true })
         easing: "easeOutExpo",
         delay: 1000
     });
+
+// Utility method to toggle display when switching between sections
+const switchSection = (sectionId) => {
+    allSections.forEach((section) => {
+        if (section.id === sectionId) {
+            section.style.display = "block";
+        } else if (section.style.display === "block") {
+            section.style.display = "none";
+        }
+    });
+};
